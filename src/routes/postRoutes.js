9b7b240a1ed9375e5152206abdc9f87528ccd5d8
sugamshrestha677 <xresthasugam@gmail.com>
2025-10-17 23:32:16 +0545

    postRoutes.js modified

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,13 +1,11 @@
-// src/routes/postRoutes.js
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const postCtrl = require('../controllers/postController');
-const auth = require('../middleware/auth');
+const postCtrl = require("../controllers/postController");
 
-router.post('/', auth, postCtrl.createPost);
-router.get('/', postCtrl.getAllPosts);
-router.get('/:id', postCtrl.getPostById);
-router.put('/:id', auth, postCtrl.updatePost);
-router.delete('/:id', auth, postCtrl.deletePost);
+router.post("/", postCtrl.createPost);
+router.get("/", postCtrl.getPosts);
+router.get("/:id", postCtrl.getPostById);
+router.put("/:id", postCtrl.updatePost);
+router.delete("/:id", postCtrl.deletePost);
 
 module.exports = router;
