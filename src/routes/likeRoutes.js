c49dbabe36cbe8d62660d5b2c87efe91185cab1f
sugamshrestha677 <xresthasugam@gmail.com>
2025-10-17 23:31:46 +0545

    likeRoutes.js modified

--- src/routes/likeRoutes.js
+++ src/routes/likeRoutes.js
@@ -1,11 +1,9 @@
-// src/routes/likeRoutes.js
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const likeCtrl = require('../controllers/likeController');
-const auth = require('../middleware/auth');
+const likeCtrl = require("../controllers/likeController");
 
-router.post('/:id/like', auth, likeCtrl.likePost);      // POST /api/posts/:id/like (we'll mount under /api/posts)
-router.delete('/:id/unlike', auth, likeCtrl.unlikePost);
-router.get('/:id/likes', likeCtrl.countLikes);
+router.post("/", likeCtrl.likePost);
+router.delete("/", likeCtrl.unlikePost);
+router.get("/:postId", likeCtrl.countLikes);
 
 module.exports = router;
